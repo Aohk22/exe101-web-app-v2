0ebf7a50869c5d4542d97d15e37215bf80d060eb
Aohk22 <96096597+Aohk22@users.noreply.github.com>
2026-03-17 18:42:47 +0700

    authentication middleware

--- app/middleware/auth.ts
+++ app/middleware/auth.ts
@@ -1,6 +1,6 @@
 import { redirect } from "react-router";
 import { userContext } from "~/context";
-import { getSession } from "~/.server/auth/sessions";
+import { destroySession, getSession } from "~/.server/auth/sessions";
 import { getUserById } from "~/.server/database/utils";
 import type {Route} from '../+types/root'
 
@@ -8,14 +8,25 @@ export const authMiddleware: Route.MiddlewareFunction = async ({
   request,
   context,
 }) => {
+  console.log('Auth middleware activating.')
   const session = await getSession(request.headers.get('Cookie'));
   // const session = await getSession();
   const userId = session.get("userId");
+  console.log(`Got userId from session: ${userId}`)
 
   if (!userId) {
     throw redirect("/login");
   }
 
   const user = await getUserById(userId);
+
+  if (!user) {
+    throw redirect('/login', {
+      headers: {
+        'Set-Cookie': await destroySession(session)
+      }
+    })
+  }
+
   context.set(userContext, user);
 };
