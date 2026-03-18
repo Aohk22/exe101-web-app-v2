36ed0aebc9a5d25d310d0559a9413c1f859e9e48
Aohk22 <96096597+Aohk22@users.noreply.github.com>
2026-03-18 22:39:31 +0700

    error handling

--- app/.server/auth/login.ts
+++ app/.server/auth/login.ts
@@ -1,26 +1,34 @@
 import bcrypt from 'bcrypt'
-import { eq } from 'drizzle-orm'
+import { DrizzleQueryError, eq } from 'drizzle-orm'
 import { db } from '~/.server/database/connection'
 import { users } from '~/.server/database/schema'
 
 async function validateCredentials(email: string, password: string) {
-	const result = await db.select().from(users).where(eq(users.email, email))
+	try {
+		const result = await db.select().from(users).where(eq(users.email, email))
 
-	const user = result.at(0)
+		const user = result.at(0)
 
-	if (!user) {
-		return null
-	}
+		if (!user) {
+			return null
+		}
 
-	if (!user.password) {
-		return null
-	}
+		if (!user.password) {
+			return null
+		}
 
-	const match = await bcrypt.compare(password, user.password)
-	if (match) {
-		return user.id
-	} else {
-		return null
+		const match = await bcrypt.compare(password, user.password)
+		if (match) {
+			return user.id
+		} else {
+			return null
+		}
+	} catch (e) {
+		if (e instanceof DrizzleQueryError) {
+			console.log('ERROR: DrizzleQueryError')
+			return null
+		}
+		throw e
 	}
 }
 
